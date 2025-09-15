2b0d2bb9825bef5b00a3dee88ff91e462f7dd832
Chris <Chrkim27@bergen.org>
2025-09-15 10:29:48 -0400

    flask done 14

---
+++ Lesson_04_Routes/app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return """
+            <h1>Dynamic Routes Demo</h1>
+<h2>Try: These URLS</h2>
+<ul>
+    <li><a href="/user/john">User Profile: John</a></li>
+    <li><a href="/user/alice">User Profile: Alice</a></li>
+</ul>
+"""
+
+@app.route('/user/<username>', methods=['GET'])
+def user_profile(username):
+    return f'''
+     <h1>User Profile</h1>
+ <p>Username: <strong>{username}</strong></p>
+ <p>Profile Type: {type(username).__name__}</p>
+ <p>Welcome to {username}'s profile page!</p>
+ <nav>
+    <a href="/"> Back to Homepage</a>
+    <a href="/user/alice">Alice</a>
+    <a href="/user/bob">Bpb</a>
+ </nav>
+'''
+
+@app.route("/calc/<int:num1>/<operation>/<int:num2>", methods=['GET'])
+def calculator(num1, operation, num2):
+    operations = {
+        '+': num1 + num2,
+        '-': num1 - num2,
+        '*': num1 * num2,
+        '/': num1 / num2 if num2 != 0 else 'Error: Division by zero!'
+    }
+    if operation in operations:
+        result = operations[operation]
+        return f"{num1} {operation} {num2} = {result}"
+    else:
+        return f"Unknown operations!{operation}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
